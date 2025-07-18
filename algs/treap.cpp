161374d7be19f093a07a022f6bac7bf556c88920
narekb95 <narekb95@gmail.com>
2025-07-18 17:14:05 +0200

    fixed treap rotation

--- algs/treap.cpp
+++ algs/treap.cpp
@@ -1,3 +1,28 @@
+/**
+ * @class Treap
+ * @brief Implements a randomized balanced binary search tree (Treap) with augmented range queries.
+ *
+ * The Treap class provides efficient insertion, deletion, and range query operations on a set of key-value pairs.
+ * Each node in the Treap is assigned a random priority, ensuring expected logarithmic time complexity for operations.
+ * The Treap is augmented to support associative and commutative aggregate queries (e.g., min, max, sum) over key ranges.
+ *
+ * Features:
+ * - Insert or upsert key-value pairs, maintaining balance via randomized priorities.
+ * - Query aggregate values over a range of keys using a user-defined combine function.
+ * - Find the k-th smallest key (order statistics).
+ * - Efficient removal of nodes.
+ * - Customizable aggregate operation and neutral value.
+ *
+ * Template Parameters:
+ * - The combine function must be associative and commutative.
+ *
+ * Usage Example:
+ * @code
+ * Treap treap([](I x, I l, I r){ return min({x, l, r}); }, LLONG_MAX);
+ * treap.insert(5, 10);
+ * I min_value = treap.query(3, 7);
+ * @endcode
+ */
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -36,15 +61,6 @@ ostream& operator <<(ostream& out, const pair<S,T>& p){
 	return out;
 }
 
-struct Pseudo{
-	// V a = {28, 19, 26, 27};
-	V a = {4, 1, 2, 3};
-	I index = 0;
-	int rand_I()
-	{
-		return a[index++];
-	}
-} prnd;
 struct Rand{
 
 	mt19937_64 gen;
@@ -54,13 +70,13 @@ struct Rand{
 	{
 		shuffle(a.begin(), a.end(), gen);
 	}
-	int rand_range(I a, I b)
+	I rand_range(I a, I b)
 	{
 		return uniform_int_distribution<I>(a, b)(gen);
 	}
-	int rand_I()
+	I rand_I()
 	{
-		return gen();
+		return uniform_int_distribution<I>(0, LLONG_MAX)(gen);
 	}
 } rnd;
 
@@ -107,6 +123,93 @@ class Treap{
 		print_node(node->right);
 		cout << "end " << node->key << endl;
 	}
+
+    void remove_node(Node* node)
+    {
+        if(node == nullptr) return;
+		// cout << "Rotating" << endl;
+		while(rotate_downwards(node));
+		// cout << "done" << endl;
+		auto parent = node->parent;
+		if(parent != nullptr)
+		{
+			if(parent->left == node)
+			{
+				parent->left = nullptr;
+			}
+			else
+			{
+				parent->right = nullptr;
+			}
+		}
+		else
+		{
+			root = nullptr;
+		}
+		delete node;
+		update_aggregate_values_up(parent);
+	}
+
+	// doesn't change aggregate values
+	// but all changes become ancestors?
+	bool rotate_downwards(Node* node) 
+	{
+		auto left = node->left;
+		auto right = node->right;
+		auto parent = node->parent;
+		if(left == nullptr && right == nullptr)
+		{
+			return false;
+		}
+		if(left == nullptr || (right != nullptr && right->priority > left->priority))
+		{
+			node->parent = right;
+			right->parent = parent;
+			node->right = right->left;
+			right->left = node;
+			if(parent == nullptr)
+			{
+				root = right;
+			}
+			else if(parent->left == node)
+			{
+				parent->left = right;
+			}
+			else
+			{
+				parent->right = right;
+			}
+		}
+		else
+		{
+			node->parent = left;
+			left->parent = parent;
+			node->left = left->right;
+			left->right = node;
+			if(parent == nullptr)
+			{
+				root = left;
+			}
+			else if(parent->left == node)
+			{
+				parent->left = left;
+			}
+			else
+			{
+				parent->right = left;
+			}
+		}
+		return true;
+	}	
+
+    void update_aggregate_values_up(Node* node)
+    {
+        while(node != nullptr)
+        {
+            update_aggregate_value(node);
+            node = node->parent;
+        }
+    }
 
 	void update_aggregate_value(Node* node)
 	{
@@ -185,6 +288,23 @@ class Treap{
 		// Also do empty query since this only returns neutral value
 		return combine(self_value, query_node(node->left, qs, qe), query_node(node->right, qs, qe));
 	}
+
+
+	Node* find_key(I key)
+	{
+		Node* node = root;
+		while(node != nullptr)
+		{
+			if(key == node->key)
+			{
+				return node;
+			}
+			node = (key < node->key ? node->left : node->right);
+		}
+		return nullptr;
+	}
+
+
 public:
 
 	Treap(
@@ -201,10 +321,23 @@ public:
 		print_node(root);
 	}
 
+	void upsert(I key, I value)
+	{
+		// cout << "searching" << endl;
+		auto* node = find_key(key);
+		if(node != nullptr)
+		{
+			// cout << "Removing" << endl;
+			value = combine(value, node->value, neutral_value);
+			remove_node(node);
+		}
+		// cout << "Inserting key: " << key << ", value: " << value << endl;
+		insert(key, value);
+	}
+
 	void insert(I key, I value)
 	{
 		I priority = rnd.rand_I();
-		cout << "Inserting " << key << " " << value << " " << priority << endl;
 		if(root == nullptr)
 		{
 			root = new Node(priority, key, value, 1, value, nullptr);
@@ -215,12 +348,9 @@ public:
 		Node* parent = nullptr;
 		while(node != nullptr)
 		{
-			// cout << node->key << " ";
-			node->size++;
 			parent = node;
 			node = key < node->key ? node->left : node->right;
 		}
-		// cout << endl;
 
 		if(key < parent->key)
 		{
@@ -233,8 +363,11 @@ public:
 
 		while(node->parent != nullptr && node->priority > node->parent->priority)
 		{
-			// cout << "Rotating upwards " << node->key << " " << node->parent->key << endl;
 			rotate_upwards(node);
+		}
+		if(node->parent == nullptr)
+		{
+			root = node;
 		}
 		while(node != nullptr)
 		{
@@ -267,19 +400,6 @@ public:
 			}
 		}
 	}
-	I find(I key)
-	{
-		Node* node = root;
-		while(node != nullptr)
-		{
-			if(key == node->key)
-			{
-				return node->value;
-			}
-			node = (key < node->key ? node->left : node->right);
-		}
-		return -1;
-	}
 
 	I query(I qs, I qe)
 	{
@@ -287,22 +407,59 @@ public:
 	}
 };
 
-void solve(I t)
+void solve()
 {
-	I neutral = - (1LL<< 60);
-	Treap treap([](I x,I l, I r){return max({x, l, r});}, neutral);
-	I n;
-	cin >> n;
-	I ind = 0;
-	while(n--)
-	{
-		string q;
-		I a, b;
-		cin >> q >> a >> b;
-		if(q == "A")
-		{
-		}
-	}
+	I neutral = (1LL<< 60);
+	I n, k;
+	cin >> n >> k;
+    V a(n);
+    cin >> a;
+    V pre(n, 0);
+    pre[0] = a[0];
+    for(I i = 1; i < n; i++)
+    {
+        pre[i] = pre[i-1] + a[i];
+    }
+
+    I min_sum = -30000 * 15000 - 10;
+    I max_sum = 30000 * 15000 + 10;
+    I start = min_sum, end = max_sum + 1;
+    while(start < end-1)
+    {
+        I m = (start + end) / 2;
+	    Treap treap([](I x,I l, I r){return min({x, l, r});}, neutral);
+        treap.upsert(0, 0);
+        bool bad = false;
+        // cout << start << " " << end << " " << m << endl;
+        for(I i = 0; i < n; i++)
+        {
+            I pref_target = pre[i] - m;
+            I bst = treap.query(pref_target, max_sum);
+            // cout <<  treap.query(pref_target - 1, max_sum) << endl;
+            // cout << "i= " << i << ", a[i]: " << a[i] << ", pre[i]: " << pre[i] << ", target: " << pref_target << ", best: " << bst << endl;
+
+            if (bst == neutral)
+            {
+                bad = true;
+                // cout << "breaking for too large value" << endl;
+                break;
+            }
+            if(i == n-1)
+                bad = (bst + 1 > k);
+			// cout << "upserting: " << pre[i] << ", bst + 1: " << bst + 1 << endl;
+            treap.upsert(pre[i], bst + 1); // allowing duplicate indices for now
+			// cout << "done" << endl;
+        }
+        if(bad)
+        {
+            start = m;
+        }
+        else
+        {
+            end = m;
+        }
+    }
+    cout << end << endl;
 }
 
 int main()
@@ -310,14 +467,12 @@ int main()
 	//Make IO fast
 	ios_base::sync_with_stdio(0);
 
-	// I t;
-	// cin >> t;
-	// for(int i = 0; i < t; i++)
-	// {
-	// 	solve(i+1);
-	// }
-
-	solve(0);
-
+ 
+    I t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
 	return 0;
 }
